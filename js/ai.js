070b173723c44f70fee321a9a3200ed9467cb97a
asif-fahad <asif123fahad@gmail.comm>
2023-03-04 11:29:29 +0600

    see more data

--- js/ai.js
+++ js/ai.js
@@ -1,16 +1,28 @@
 const loadMyAI = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/ai/tools");
     const data = await res.json();
-    // console.log(data);
-    showAllCards(data);
+    // console.log(data.data.tools);
+    showAllCards(data.data.tools.slice(0, 6));
 };
 loadMyAI();
 
 const showAllCards = (cards) => {
+    
     // console.log(cards.data.tools[0])
     const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML = ' ';
+    
+    const showAll = document.getElementById('show-all');
+    if(cards.length === 6) {
+        
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
+    
     // console.log(cards)
-    cards.data.tools.forEach(card => {
+    cards.forEach(card => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('col');
         cardDiv.innerHTML = `
@@ -34,9 +46,33 @@ const showAllCards = (cards) => {
     </div>
     </div>
     `;
-        cardsContainer.appendChild(cardDiv);
+    cardsContainer.appendChild(cardDiv);
+    toggleSpinner(false)
     });
 }
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
+
+const seeMoreData = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/ai/tools");
+    const data = await res.json();
+    // console.log(data.data.tools);
+    showAllCards(data.data.tools);
+}
+
+// document.getElementById('btn-show-all').addEventListener('click', function(){
+//     toggleSpinner(true)
+//     showAllCards(cards)
+// })
+
 
 const fetchModalDetail = (id) => {
     let url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -52,18 +88,18 @@ const showModalDetail = (modalDetail) => {
                 <div class="col">
                   <div class="card">
                     <div class="card-body bg-danger p-2 text-dark bg-opacity-10">
-                      <h5 class="card-title">${modalDetail.description}</h5>
+                      <h5 class="card-title">${modalDetail.description === null ? " " : modalDetail.description}</h5>
                       <div class="d-flex gap-1">
                         <div class="border bg-white text-success">
-                        ${modalDetail.pricing[0].price}
+                        ${modalDetail.pricing[0].price ? modalDetail.pricing[0].price : "Free of Cost"}
                         ${modalDetail.pricing[0].plan}
                         </div>
                         <div class="border bg-white text-primary-emphasis">
-                        ${modalDetail.pricing[1].price}
+                        ${modalDetail.pricing[1].price ? modalDetail.pricing[1].price : "Free of Cost"}
                         ${modalDetail.pricing[1].plan}
                         </div>
                         <div class="border bg-white text-danger-emphasis">
-                        ${modalDetail.pricing[2].price}
+                        ${modalDetail.pricing[2].price ? modalDetail.pricing[2].price : "Free of Cost"}
                         ${modalDetail.pricing[2].plan}
                         </div>
                       </div>
@@ -71,17 +107,17 @@ const showModalDetail = (modalDetail) => {
                         <div>
                         <h5>Features</h5>
                         <ul>
-                            <li>${modalDetail.features[1].feature_name}</li>
-                            <li>${modalDetail.features[2].feature_name}</li>
-                            <li>${modalDetail.features[3].feature_name}</li>
+                            <li>${modalDetail.features[1].feature_name ? modalDetail.features[1].feature_name : "No Data Found"}</li>
+                            <li>${modalDetail.features[2].feature_name ? modalDetail.features[1].feature_name : "No Data Found"}</li>
+                            <li>${modalDetail.features[3].feature_name ? modalDetail.features[1].feature_name : "No Data Found"}</li>
                         </ul>
                         </div>
                         <div>
                         <h5>Integrations</h5>
                         <ul>
-                            <li>${modalDetail.integrations[0]}</li>
-                            <li>${modalDetail.integrations[1]}</li>
-                            <li>${modalDetail.integrations[2]}</li>
+                            <li>${modalDetail.integrations[0] ? modalDetail.integrations[0] : "No Data Found"}</li>
+                            <li>${modalDetail.integrations[1] ? modalDetail.integrations[0] : "No Data Found"}</li>
+                            <li>${modalDetail.integrations[2] ? modalDetail.integrations[0] : "No Data Found"}</li>
                         </ul>
                         </div>
                       </div>
@@ -91,8 +127,8 @@ const showModalDetail = (modalDetail) => {
                 <div class="col">
                   <div class="card">
                   
-                    <img id="modal-img" src="${modalDetail.image_link[0]}" class="card-img-top" alt="...">
-                    <div class="fs-6 fw-light text-white bg-danger position-absolute top-0 end-0">${modalDetail.accuracy.score} accuracy</div>
+                    <img id="modal-img" src="${modalDetail.image_link[0] ? modalDetail.image_link[0] : " "}" class="card-img-top" alt="...">
+                    <div id="accuracy-btn" class="fs-6 fw-light text-white bg-danger position-absolute top-0 end-0">${modalDetail.accuracy.score*100 ? modalDetail.accuracy.score*100 : ' '}% accuracy</div>
                     <div class="card-body text-center">
                       <h5 class="card-title">${modalDetail.input_output_examples[0].input}
                       </h5>
